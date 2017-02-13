53c2f4c355ba2855da4ffba0e7c793d938e43438
Christian Duffett <duffett@gmail.com>
2017-02-12 22:06:37 -0500

    .\

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ var hbs = exphbs.create({
 app.engine('handlebars', hbs.engine);
 
 // view engine setup
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join('.', '/views'));
 app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
