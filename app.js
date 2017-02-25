4418ea5193a253da78400ca65106e37d5217b13e
Christian Duffett <duffett@gmail.com>
2017-02-24 21:05:47 -0500

    not working with first passport package

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var path = require('path');
+var passport = require('passport');
+var util = require('util')
+var IntuitStrategy = require('passport-intuit').Strategy;
 var exphbs = require('express-handlebars');
 var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser')
-var cookieSession = require('cookie-session')
+var session = require('express-session')
 
+var cookieParser = require('cookie-parser')
+// var cookieSession = require('cookie-session')
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
 var index = require('./routes/index');
 var users = require('./routes/users');
 var routes = require('./routes/index');
@@ -12,6 +18,49 @@ var invoiceRoute = require('./routes/invoice.js');
 var docsRoute = require('./routes/docs.js');
 var connect = require('./routes/oauth/connect');
 var callback = require('./routes/oauth/callback');
+
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Intuit profile is serialized
+//   and deserialized.
+passport.serializeUser(function (user, done) {
+  console.log('serialize: ' + JSON.stringify(user, null, 4))
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  console.log('deserialize: ' + JSON.stringify(user, null, 4))
+  done(null, obj);
+});
+
+// Use the IntuitStrategy within Passport.
+//   Strategies in passport require a `validate` function, which accept
+//   credentials (in this case, an OpenID identifier and profile), and invoke a
+//   callback with a user object.
+var hostName = 'localhost'
+passport.use(new IntuitStrategy({
+    returnURL: 'http://' + hostName + ':' + '3000' + '/auth/intuit/return',
+    realm: 'http://' + hostName + ':' + '3000' + '/'
+  },
+  function (identifier, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Intuit profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Intuit account with a user record in your database,
+      // and return that user instead.
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
+  }
+));
+
 
 // eventually resolve all of these issues with 
 // keeping the entry point stored in web.config 
@@ -36,12 +85,12 @@ var addQboSettings = function (req, res, next) {
   //   oauth_token: 'lvprdPWqxtUv9Gd9CMFYO6CR6I6qElYWfQL1HJnveQuQ980Y'
   // }
 
-  req.session.qbo = {
-    token: accessToken.oauth_token,
-    secret: accessToken.oauth_token_secret,
-    companyid: postBody.oauth.realmId
-  };
-  console.log("VALUES TO UPDATE ~30 DAYS: " + JSON.stringify(req.session.qbo))
+  // req.session.qbo = {
+  //   token: accessToken.oauth_token,
+  //   secret: accessToken.oauth_token_secret,
+  //   companyid: postBody.oauth.realmId
+  // };
+  // console.log("VALUES TO UPDATE ~30 DAYS: " + JSON.stringify(req.session.qbo))
   next(); // Passing the request to the next handler in the stack.
 }
 
@@ -89,23 +138,86 @@ app.engine('handlebars', hbs.engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
+app.use(cookieParser())
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(session({
+  secret: 'keyboard cat'
+}));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1']
-}))
+// app.use(cookieSession({
+//   name: 'session',
+//   keys: ['key1']
+// }))
 
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/connect', connect);
-app.use('/callback', callback);
-app.use('/display', routes);
-app.use('/invoice', invoiceRoute);
+app.get('/', function (req, res) {
+  console.log('/')
+  console.log(JSON.stringify(req.user));
+  console.log('render')
+  res.render('index', {
+    user: req.user
+  });
+});
+// app.use('/', index);
+// app.use('/connect', connect);
+// app.use('/callback', callback);
+// app.use('/display', routes);
+// app.use('/invoice', invoiceRoute);
+
+app.get('/account', ensureAuthenticated, function (req, res) {
+  res.render('account', {
+    user: req.user
+  });
+});
+
+app.get('/login', function (req, res) {
+  res.render('login', {
+    user: req.user
+  });
+});
+
+// GET /auth/intuit
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Intuit authentication will involve redirecting
+//   the user to intuit.com.  After authenticating, Intuit will redirect the
+//   user back to this application at /auth/intuit/return
+app.get('/auth/intuit',
+  passport.authenticate('intuit', {
+    failureRedirect: '/login'
+  }),
+  function (req, res) {
+    console.log('here we are...')
+    res.redirect('/');
+  });
+
+// GET /auth/intuit/return
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/intuit/return',
+  passport.authenticate('intuit', {
+    failureRedirect: '/login'
+  }),
+  function (req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -124,5 +236,15 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login')
+}
 
 module.exports = app;
