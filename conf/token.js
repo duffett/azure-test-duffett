3d6fe8544f953214e670f025d535465a03be5c6b
Christian Duffett <duffett@gmail.com>
2017-02-27 21:39:03 -0500

    oh well

---
+++ conf/token.js
@@ -0,0 +1,12 @@
+  module.exports = {
+      "consumer": {
+          "key": "qyprdMpBKxDTNpdezFyTnatBlsLlxo",
+          "secret": "VzatEPZ2cupgWNR5BWeZCY1t5jjHiLktHNGFoM1I"
+      },
+      "oauth": {
+          "token": "lvprdHJfDNXXcbst2FYxcYzFRSl1lQ7KdTbMuYVGrOztpYID",
+          "secret": "XX8E8gy1QEFFDUUPTSGPsGzltF0ededLSRZDTTpA"
+      },
+      "companyId": "123145749850059"
+
+  }
